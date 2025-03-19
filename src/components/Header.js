2e1d4220ac3fc7e03a85e44eb79e13d6cacaa556
Juan Felipe Fernandez <ferjuanfelipe7@gmail.com>
2025-03-19 16:44:04 -0500

    Creacion del proyecto de una pagina

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Hospital</h1>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome to Hospital</h2>
+            <p>Your health is our priority.</p>
+        </div>
+    );
+};
+
+export default Home;
