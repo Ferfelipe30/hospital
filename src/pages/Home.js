ac5aeda9111de363739fa059d5c1ae7a17136dde
Emersonk24 <emerpachon206@gmail.com>
2025-03-28 23:37:43 -0500

    Guardando cambios antes del pull

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React from 'react';
+import ServicesGrid from '../components/Services/ServicesGrid';
+import HeroCarousel from '../components/Hero/HeroCarousel';
 
 const Home = () => {
-    return (
-        <div>
-            <h2>Welcome to Hospital</h2>
-            <p>Your health is our priority.</p>
-        </div>
-    );
+  return (
+    <main>
+      <HeroCarousel />
+      <section className="py-16 container mx-auto px-6">
+        <h2 className="text-3xl font-bold text-gray-800 mb-8">Nuestros Servicios Destacados</h2>
+        <ServicesGrid />
+      </section>
+    </main>
+  );
 };
 
 export default Home;
