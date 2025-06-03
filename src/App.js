be33a2aaeefc0be91a7091a5c82d6a456976f55b
Juan Felipe Fernandez <ferjuanfelipe7@gmail.com>
2025-06-03 17:50:24 -0500

    Correcion del despliegue 3

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       <Header />
       <main>
         <Routes>
-          <Route path='*' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/PatientRegistration' element={<PatientRegistration />} />
           <Route path='/login' element={<Login />} />
